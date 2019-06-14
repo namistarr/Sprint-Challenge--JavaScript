5ccfad909df621696f2579fb29418c4cbdd0bdbd
namistarr <namistarr@icloud.com>
2019-06-14 09:52:11 -0700

    functions.js worked on

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,14 +6,24 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function consume(argument1, argument2, cb) {
+  console.log(cb(argument1, argument2)) ;
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
+function add(num1, num2) {
+  return num1 + num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function greeting(first_name, last_name) {
+  return `Hello ${first_name} ${last_name}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // consume(2,2,add); // 4
@@ -25,7 +35,7 @@
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: Closure gives you access to an outer function's scope from an inner function.
 
 
 const external = "I'm outside the function";
